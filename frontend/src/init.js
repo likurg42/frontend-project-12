0df6f8b9a2839d52375e5bcbb39ae72bdc028491
likurg42 <likurg42@gmail.com>
2023-01-20 00:28:47 +0300

    chore: remove test error

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -16,11 +16,6 @@ const init = () => {
     environment: 'testenv',
   };
 
-  function TestError() {
-    const a = null;
-    return a.hello();
-  }
-
   const i18n = initI18n();
   const socket = initSocket();
   const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -29,7 +24,6 @@ const init = () => {
     <React.StrictMode>
       <RollbarProvider config={rollBarConfig}>
         <ErrorBoundary>
-          <TestError />
           <AuthProvider>
             <ChatProvider socket={socket}>
               <ReduxProvider store={store}>
