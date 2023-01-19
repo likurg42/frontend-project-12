a7260c987222c1338ab02d39cc7a99b5bf9682cd
likurg42 <likurg42@gmail.com>
2023-01-20 00:13:08 +0300

    feat: add rollbar

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { AuthProvider } from './contexts/authContext.js';
 import { ChatProvider } from './contexts/chatContext.js';
 import store from './slices/index.js';
@@ -10,21 +11,30 @@ import initSocket from './socket/socket.js';
 import initI18n from './i18n/i18n.js';
 
 const init = () => {
+  const rollBarConfig = {
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    environment: 'testenv',
+  };
+
   const i18n = initI18n();
   const socket = initSocket();
   const root = ReactDOM.createRoot(document.getElementById('root'));
 
   root.render(
     <React.StrictMode>
-      <AuthProvider>
-        <ChatProvider socket={socket}>
-          <ReduxProvider store={store}>
-            <I18nextProvider i18n={i18n}>
-              <App />
-            </I18nextProvider>
-          </ReduxProvider>
-        </ChatProvider>
-      </AuthProvider>
+      <RollbarProvider config={rollBarConfig}>
+        <ErrorBoundary>
+          <AuthProvider>
+            <ChatProvider socket={socket}>
+              <ReduxProvider store={store}>
+                <I18nextProvider i18n={i18n}>
+                  <App />
+                </I18nextProvider>
+              </ReduxProvider>
+            </ChatProvider>
+          </AuthProvider>
+        </ErrorBoundary>
+      </RollbarProvider>
     </React.StrictMode>,
   );
 };
